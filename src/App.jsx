4934dd4c71aa520e87e4a7246385924809c3409a
Titus Njema <njema3@gmail.com>
2024-08-29 09:36:45 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Blogs from "./pages/Blogs";
+import Comments from "./pages/Comments";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route index element={<Home />} /> 
+      <Route index element={<About />} /> 
+      <Route index element={<Comments />} /> 
+      <Route index element={<Blogs />} /> 
       
     </>
   )
