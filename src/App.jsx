195cae38af834c5e53c68646da0072b9a7f71396
Titus Njema <njema3@gmail.com>
2024-08-29 09:24:12 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
-import Navbar from "./components/Navbar"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route index element={<HomePage />} /> 
+      <Route path="/login" element={<Login />} /> 
+      <Route path="/register" element={<Register />} /> 
+      <Route path="/dashboard" element={<MainLayout/>} >            
+        <Route path="/dashboard" element={<Dashboard />} />  
+      </Route>
+    </>
+  )
+);
 
 
 function App() {
@@ -6,7 +24,7 @@ function App() {
 
   return (
     <>    
-      <Navbar />
+      <RouterProvider router={router}/>
       
     </>
   )
