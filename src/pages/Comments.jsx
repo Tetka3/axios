41f2639aa4b1b3481aed43a2f6a0973b9dd2b27b
Titus Njema <njema3@gmail.com>
2024-08-30 07:59:09 +0300

    first commit

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 
 const Comments = () => {
+
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    
+  })
   return (
     <div>
       <Navbar/>
       <div>
         {
-          
+
         }
       </div> 
     </div>
