25dd64b55e6571da805f370d4c933ca36763149e
Titus Njema <njema3@gmail.com>
2024-08-31 10:08:36 +0300

    first commit

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import axios from "axios"
 
 const Blogs = () => {
 
   const [posts, setPosts] = useState([]);
-  
+
+  const fetchPosts = () => {
+    
+  }
+
+  useEffect(() => {
+
+  })
+
   return (
     <div>
       <Navbar/>
